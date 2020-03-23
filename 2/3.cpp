facd7940bb7fed193f9b680eb01f20ffa88c91e8
Patryk Szwed <patryk.szwed@outlook.com>
2020-03-23 11:47:22 +0100

    3rd - fixed

--- 2/3.cpp
+++ 2/3.cpp
@@ -28,16 +28,11 @@ int main(int argc, char **argv) {
         addNumber(number, number_to_add, first_process);
         sendNumber(number, first_process, first_process + 1);
         receiveNumber(number, last_process, first_process);
-    } else if (process_number == last_process) {
-        receiveNumber(number, last_process - 1, last_process);
-        number_to_add = getRandomNumber();
-        addNumber(number, number_to_add, last_process);
-        sendNumber(number, last_process, first_process);
     } else {
         receiveNumber(number, process_number - 1, process_number);
         number_to_add = getRandomNumber();
         addNumber(number, number_to_add, process_number);
-        sendNumber(number, process_number, process_number + 1);
+        sendNumber(number, process_number, (process_number +1) % number_of_processes);
     }
 
     MPI_Finalize();
@@ -45,12 +40,16 @@ int main(int argc, char **argv) {
 }
 
 void sendNumber(int& number_to_send, int source_process, int destination_process) {
+    printf("Process with number %d will send a number to the process with number %d \n",
+           source_process, destination_process);
     MPI_Send(&number_to_send, 1, MPI_INT, destination_process, 0, MPI_COMM_WORLD);
     printf("Process with number %d sent number %d to the process with number %d \n",
            source_process, number_to_send, destination_process);
 }
 
 void receiveNumber(int& variable_to_save, int source_process, int destination_process) {
+    printf("Process with number %d will receive a number from the process with number %d \n",
+           destination_process, source_process);
     MPI_Recv(&variable_to_save, 1, MPI_INT, source_process, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     printf("Process with number %d received number %d from the process with number %d \n",
            destination_process, variable_to_save, source_process);
